5fe12d417a18f13219424aa68c4ac85b40f467e1
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2016-04-03 15:20:23 +0100

    add interface to broker and transporter

--- broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
+++ broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
@@ -1,6 +1,40 @@
 package pt.upa.broker.ws;
 
-public class BrokerPort {
+import java.util.List;
+
+public class BrokerPort implements BrokerPortType {
+
+	@Override
+	public String ping(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String requestTransport(String origin, String destination, int price)
+			throws InvalidPriceFault_Exception, UnavailableTransportFault_Exception,
+			UnavailableTransportPriceFault_Exception, UnknownLocationFault_Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TransportView viewTransport(String id) throws UnknownTransportFault_Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TransportView> listTransports() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clearTransports() {
+		// TODO Auto-generated method stub
+		
+	}
 
 	// TODO
 
--- transporter-ws/src/main/java/pt/upa/transporter/ws/TransporterPort.java
+++ transporter-ws/src/main/java/pt/upa/transporter/ws/TransporterPort.java
@@ -1,6 +1,45 @@
 package pt.upa.transporter.ws;
 
-public class TransporterPort {
+import java.util.List;
+
+public class TransporterPort implements TransporterPortType {
+
+	@Override
+	public String ping(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public JobView requestJob(String origin, String destination, int price)
+			throws BadLocationFault_Exception, BadPriceFault_Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public JobView decideJob(String id, boolean accept) throws BadJobFault_Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public JobView jobStatus(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<JobView> listJobs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clearJobs() {
+		// TODO Auto-generated method stub
+		
+	}
 
 	// TODO
 
