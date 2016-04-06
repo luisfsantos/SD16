b01753bfd66385b6ebb27d83cbcca1049780358e
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2016-04-06 23:46:44 +0100

    fix pom.xml; add TransporterPortType interface to TransporterClient TransporterClient now implements the TransporterPortType interface and creates the stub necessary to make the invocation to TransporterClient

--- transporter-ws-cli/src/main/java/pt/upa/transporter/TransporterClientApplication.java
+++ transporter-ws-cli/src/main/java/pt/upa/transporter/TransporterClientApplication.java
@@ -1,58 +1,20 @@
 package pt.upa.transporter;
 
-import static javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY;
-
-import java.util.Map;
-
-import javax.xml.ws.BindingProvider;
-
-import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
-import pt.upa.transporter.ws.TransporterPortType;
-import pt.upa.transporter.ws.TransporterService;
+import pt.upa.transporter.ws.cli.TransporterClient;
 
 public class TransporterClientApplication {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println(TransporterClientApplication.class.getSimpleName() + " starting...");
 		// Check arguments
-		if (args.length < 2) {
+		if (args.length < 1) {
 			System.err.println("Argument(s) missing!");
-			System.err.printf("Usage: java %s uddiURL name%n", TransporterClientApplication.class.getName());
+			System.err.printf("Usage: java %s wsURL%n", TransporterClientApplication.class.getName());
 			return;
 		}
 
-		String uddiURL = args[0];
-		String name = args[1]+"1";
-
-		System.out.printf("Contacting UDDI at %s%n", uddiURL);
-		UDDINaming uddiNaming = new UDDINaming(uddiURL);
-
-		System.out.printf("Looking for '%s'%n", name);
-		String endpointAddress = uddiNaming.lookup(name);
-
-		if (endpointAddress == null) {
-			System.out.println("Not found!");
-			return;
-		} else {
-			System.out.printf("Found %s%n", endpointAddress);
-		}
-
-		System.out.println("Creating stub ...");
-		TransporterService service = new TransporterService();
-		TransporterPortType port = service.getTransporterPort();
-
-		System.out.println("Setting endpoint address ...");
-		BindingProvider bindingProvider = (BindingProvider) port;
-		Map<String, Object> requestContext = bindingProvider.getRequestContext();
-		requestContext.put(ENDPOINT_ADDRESS_PROPERTY, endpointAddress);
-
-		try {
-			String result = port.ping("friend");
-			System.out.println(result);
-
-		} catch (Exception pfe) {
-			System.out.println("Caught: " + pfe);
-		}
+		TransporterClient port = new TransporterClient(args[0]);
+		System.out.println(port.ping("testing..."));
 
 	}
 }
--- transporter-ws-cli/src/main/java/pt/upa/transporter/ws/cli/TransporterClient.java
+++ transporter-ws-cli/src/main/java/pt/upa/transporter/ws/cli/TransporterClient.java
@@ -1,7 +1,66 @@
 package pt.upa.transporter.ws.cli;
 
-public class TransporterClient {
+import static javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY;
 
-	// TODO
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.ws.BindingProvider;
+
+import pt.upa.transporter.ws.BadJobFault_Exception;
+import pt.upa.transporter.ws.BadLocationFault_Exception;
+import pt.upa.transporter.ws.BadPriceFault_Exception;
+import pt.upa.transporter.ws.JobView;
+import pt.upa.transporter.ws.TransporterPortType;
+import pt.upa.transporter.ws.TransporterService;
+
+public class TransporterClient implements TransporterPortType {
+	private TransporterPortType port;
+	
+	public TransporterClient(String endpointAddress) {
+		TransporterService service = new TransporterService();
+		port = service.getTransporterPort();
+		BindingProvider bindingProvider = (BindingProvider) port;
+		Map<String, Object> requestContext = bindingProvider.getRequestContext();
+		requestContext.put(ENDPOINT_ADDRESS_PROPERTY, endpointAddress);
+	}
+	
+	@Override
+	public String ping(String name) {
+		// TODO Auto-generated method stub
+		return port.ping(name);
+	}
+
+	@Override
+	public JobView requestJob(String origin, String destination, int price)
+			throws BadLocationFault_Exception, BadPriceFault_Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public JobView decideJob(String id, boolean accept) throws BadJobFault_Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public JobView jobStatus(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<JobView> listJobs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clearJobs() {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
