de73c0bc2523f74ad88259fff06f3e709e584a3a
S-4-S <z4swlf@gmail.com>
2016-04-11 10:39:44 +0100

    fix BrokerClient to create stub for server
    add gitignore for uddi-naming

--- broker-ws-cli/src/main/java/pt/upa/broker/ws/cli/BrokerClient.java
+++ broker-ws-cli/src/main/java/pt/upa/broker/ws/cli/BrokerClient.java
@@ -1,6 +1,71 @@
 package pt.upa.broker.ws.cli;
 
-public class BrokerClient {
+import static javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.registry.JAXRException;
+import javax.xml.ws.BindingProvider;
+
+import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
+import pt.upa.broker.ws.BrokerPortType;
+import pt.upa.broker.ws.BrokerService;
+import pt.upa.broker.ws.InvalidPriceFault_Exception;
+import pt.upa.broker.ws.TransportView;
+import pt.upa.broker.ws.UnavailableTransportFault_Exception;
+import pt.upa.broker.ws.UnavailableTransportPriceFault_Exception;
+import pt.upa.broker.ws.UnknownLocationFault_Exception;
+import pt.upa.broker.ws.UnknownTransportFault_Exception;
+
+
+
+public class BrokerClient implements BrokerPortType {
+	
+	private BrokerPortType port;
+	
+	public BrokerClient(String uddiURL, String name) throws JAXRException {
+		UDDINaming uddiNaming = new UDDINaming(uddiURL);
+		String endpointAddress = uddiNaming.lookup(name);
+		
+		BrokerService service = new BrokerService();
+		port = service.getBrokerPort();
+		BindingProvider bindingProvider = (BindingProvider) port;
+		Map<String, Object> requestContext = bindingProvider.getRequestContext();
+		requestContext.put(ENDPOINT_ADDRESS_PROPERTY, endpointAddress);
+	}
+
+	@Override
+	public String ping(String name) {
+		// TODO Auto-generated method stub
+		return port.ping(name);
+	}
+
+	@Override
+	public String requestTransport(String origin, String destination, int price)
+			throws InvalidPriceFault_Exception, UnavailableTransportFault_Exception,
+			UnavailableTransportPriceFault_Exception, UnknownLocationFault_Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TransportView viewTransport(String id) throws UnknownTransportFault_Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TransportView> listTransports() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clearTransports() {
+		// TODO Auto-generated method stub
+		
+	}
 
 	// TODO
 
