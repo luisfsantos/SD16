d4d3ea8dcef672980821001bf8d2d568e9b8acfa
pedrofffernandes <pedro.f.fernandes@tecnico.ulisboa.pt>
2016-04-14 17:08:40 +0100

    Create several tests for Transporter-ws

--- transporter-ws/src/test/java/pt/upa/transporter/ws/ExampleTest.java
+++ transporter-ws/src/test/java/pt/upa/transporter/ws/ExampleTest.java
@@ -50,4 +50,63 @@ public class ExampleTest {
         // if the assert fails, the test fails
     }
 
+
+	@Test
+	public void ping() {
+		TransporterPort transporterPort = new TransporterPort("UpaTransporter1");
+		String ping = transporterPort.ping("B");
+		assertEquals(ping, "UpaTransporter1: B");
+	}
+
+	@Test
+	public void requestJobNull() throws BadLocationFault_Exception, BadPriceFault_Exception {
+		TransporterPort transporterPort = new TransporterPort("UpaTransporter2");
+		JobView jobView = transporterPort.requestJob("Porto", "Braga", 150);
+		assertNull(jobView);
+	}
+
+	@Test(expected = BadPriceFault_Exception.class)
+	public void requestJobBadPrice() throws BadLocationFault_Exception, BadPriceFault_Exception {
+		TransporterPort transporterPort = new TransporterPort("UpaTransporter2");
+		transporterPort.requestJob("Porto", "Braga", -1);
+	}
+
+	@Test
+	public void requestJobNorthSuccess() throws BadLocationFault_Exception, BadPriceFault_Exception {
+		TransporterPort transporterPort = new TransporterPort("UpaTransporter2");
+		JobView jobView = transporterPort.requestJob("Porto", "Lisboa", 50);
+		assertEquals("UpaTransporter2", jobView.getCompanyName());
+		assertEquals("Porto", jobView.getJobOrigin());
+		assertEquals("Lisboa", jobView.getJobDestination());
+		assertEquals(JobStateView.PROPOSED, jobView.getJobState());
+	}
+
+	@Test(expected = BadLocationFault_Exception.class)
+	public void requestJobNorthOriginBadLocation() throws BadLocationFault_Exception, BadPriceFault_Exception {
+		TransporterPort transporterPort = new TransporterPort("UpaTransporter2");
+		transporterPort.requestJob("Setubal", "Porto", 50);
+	}
+
+	@Test(expected = BadLocationFault_Exception.class)
+	public void requestJobNorthDestinationBadLocation() throws BadLocationFault_Exception, BadPriceFault_Exception {
+		TransporterPort transporterPort = new TransporterPort("UpaTransporter2");
+		transporterPort.requestJob("Porto", "Setubal", 50);
+	}
+
+	@Test(expected = BadLocationFault_Exception.class)
+	public void requestJobSouthOriginBadLocation() throws BadLocationFault_Exception, BadPriceFault_Exception {
+		TransporterPort transporterPort = new TransporterPort("UpaTransporter1");
+		transporterPort.requestJob("Porto", "Setubal", 50);
+	}
+
+	@Test(expected = BadLocationFault_Exception.class)
+	public void requestJobSouthDestinationBadLocation() throws BadLocationFault_Exception, BadPriceFault_Exception {
+		TransporterPort transporterPort = new TransporterPort("UpaTransporter1");
+		transporterPort.requestJob("Setubal", "Porto", 50);
+	}
+
+
+
+
+
 }
