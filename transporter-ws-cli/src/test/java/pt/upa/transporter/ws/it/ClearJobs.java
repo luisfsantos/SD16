d3261ec1d3c8991b2d291551141dfaaa89688f6c
pedrofffernandes <pedro.f.fernandes@tecnico.ulisboa.pt>
2016-04-15 21:22:44 +0100

    Create IT classes

---
+++ transporter-ws-cli/src/test/java/pt/upa/transporter/ws/it/ClearJobs.java
@@ -0,0 +1,26 @@
+package pt.upa.transporter.ws.it;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by lads on 15-04-2016.
+ */
+public class ClearJobs extends AbstractTwsTI {
+
+	@Before
+	public void setUp() {
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	@Test
+	public void test() {
+
+		// assertEquals(expected, actual);
+		// if the assert fails, the test fails
+	}
+}
