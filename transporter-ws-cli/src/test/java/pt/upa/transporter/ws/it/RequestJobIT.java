869be4b19f03501cb57c182f099c2859de0dec6f
pedrofffernandes <pedro.f.fernandes@tecnico.ulisboa.pt>
2016-04-15 22:38:34 +0100

    Change ExampleTest in transporter to TransporterWSTest

--- transporter-ws-cli/src/test/java/pt/upa/transporter/ws/it/RequestJobIT.java
+++ transporter-ws-cli/src/test/java/pt/upa/transporter/ws/it/RequestJobIT.java
@@ -3,6 +3,15 @@ package pt.upa.transporter.ws.it;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import pt.upa.transporter.ws.BadLocationFault_Exception;
+import pt.upa.transporter.ws.BadPriceFault_Exception;
+import pt.upa.transporter.ws.JobStateView;
+import pt.upa.transporter.ws.JobView;
+import pt.upa.transporter.ws.cli.TransporterClient;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 public class RequestJobIT extends AbstractTwsIT {
 
@@ -12,6 +21,7 @@ public class RequestJobIT extends AbstractTwsIT {
 
 	@After
 	public void tearDown() {
+		transporterClient1.clearJobs();
 	}
 
 	@Test
@@ -20,4 +30,31 @@ public class RequestJobIT extends AbstractTwsIT {
 		// assertEquals(expected, actual);
 		// if the assert fails, the test fails
 	}
+
+	@Test
+	public void requestJobNull() throws BadLocationFault_Exception, BadPriceFault_Exception {
+		JobView jobView = transporterClient1.requestJob("Porto", "Braga", 150);
+		assertNull(jobView);
+	}
+
+	@Test(expected = BadPriceFault_Exception.class)
+	public void requestJobBadPrice() throws BadLocationFault_Exception, BadPriceFault_Exception {
+		transporterClient1.requestJob("Porto", "Braga", -1);
+	}
+
+	@Test
+	public void requestJobSouthSuccess() throws BadLocationFault_Exception, BadPriceFault_Exception {
+		JobView jobView = transporterClient1.requestJob("Lisboa", "Beja", 50);
+		assertEquals("UpaTransporter1", jobView.getCompanyName());
+		assertEquals("Lisboa", jobView.getJobOrigin());
+		assertEquals("Beja", jobView.getJobDestination());
+		assertEquals(JobStateView.PROPOSED, jobView.getJobState());
+	}
+
+	@Test(expected = BadLocationFault_Exception.class)
+	public void requestJobNorthOriginBadLocation() throws BadLocationFault_Exception, BadPriceFault_Exception {
+		JobView jobView = transporterClient1.requestJob("Amsterdam", "Porto", 50);
+		assertNull(jobView);
+	}
+
 }
